1dbcebd7ddc7d46035d5839cc92e317d2ed74a29
s.folorunsho <s.folorunsho@mytaxi.com>
2017-11-22 17:57:49 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hdr',
+      version='1.0.0',
+      description='Recreaction of Lecun work',
+      author='Folorunsho Solomon Opeyemi',
+      author_email='solomonope@gmail.com',
+      url='https://github.com/solomonope/hdr',
+      license='MIT')
